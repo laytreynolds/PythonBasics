cdf47d1b57e9bcb776a5b54f0b4d51945cd280e3
laytreynolds <laytreynolds@hotmail.com>
2021-08-27 16:11:09 +0100

    Second Commit

---
+++ Python basics.py
@@ -0,0 +1,50 @@
+# Declare, add and print variables 
+
+firstname = "layton"
+lastname = "reynolds"
+age = 23
+print("Hello" + " " + firstname + " " + lastname + "," + " " + "I can see that you are 23 Years old.")
+
+# Use the input function
+# Store intut from user as variable
+
+name = input("What is your Name? ")
+
+print('Hello ' + name)
+
+ # Create variable
+ # Store input from user as variable
+ # Convert str to int
+ # Store calculations as variable
+
+birth_year = input("Enter your birth year: ")
+age = 2021 - int(birth_year)
+print(age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
