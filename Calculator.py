ddec7fc853405c6939039ea998562aa2156e22e7
laytreynolds <79116038+laytreynolds@users.noreply.github.com>
2021-08-28 16:59:18 +0100

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+# Calculator
+    # Converting output to float
+    # Adding outputs
+    # Convert sum back to string
+    # Print output
+
+first = float(input("First "))
+second = float(input("Second "))
+
+sum = first * second
+
+print("Sum " + str(sum))
